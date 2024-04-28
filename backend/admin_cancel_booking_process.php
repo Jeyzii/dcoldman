3610c02c7586b0f62d14b10e9564819ca12f4d62
Lok <jc_bart18@yahoo.com>
2024-04-29 02:21:23 +0800

    manpower updates

--- backend/admin_cancel_booking_process.php
+++ backend/admin_cancel_booking_process.php
@@ -11,6 +11,25 @@ require '../vendor/phpmailer/phpmailer/src/SMTP.php';
 if (isset($_GET['booking_id']) && is_numeric($_GET['booking_id'])) {
     $bookingId = $_GET['booking_id'];
 
+        // Get all users with role = 'manpower', availability = 3, and booking_reference_id of the booking
+    $manpowerQuery = "SELECT * FROM users WHERE role = 'manpower' AND availability = 3 AND booking_reference_id = '$bookingId'";
+    $manpowerQueryResult = mysqli_query($conn, $manpowerQuery);
+
+    if ($manpowerQueryResult) {
+        //Update the availability to 1 and booking_reference_id to null for these users
+        while ($row = mysqli_fetch_assoc($manpowerQueryResult)) {
+            $userIdManpower = $row['user_id'];
+            $updateQueryManpower = "UPDATE users SET availability = 1, booking_reference_id = NULL WHERE user_id = $userIdManpower";
+            $updateResultManpower = mysqli_query($conn, $updateQueryManpower);
+            
+            if (!$updateResultManpower) {
+                $_SESSION['error'] = "Error updating user: " . mysqli_error($conn);
+            }
+        }
+    } else {
+        $_SESSION['error'] = "Error: " . mysqli_error($conn);
+    }
+
     // Update the booking status to 'approved'
     $updateQuery = "UPDATE bookings SET status = 'cancel', management_approval = '0' WHERE booking_id = $bookingId";
     $updateResult = mysqli_query($conn, $updateQuery);
